b6cc3219077746be8d37f828e4fbb93f049c085e
J-arv-is <dhruv.singh.cse20@itbhu.ac.in>
2021-06-28 17:37:16 +0530

    Added first player X only

--- script.js
+++ script.js
@@ -17,7 +17,140 @@ let won=false;
 let ai=0;
 let player=0;
 let favour=0;
+let chance=1;
 var board=[[0,0,0],[0,0,0],[0,0,0]];
+let ini=0;
+
+function min(a,b)
+{
+  if(a<b) return a;
+  else return b;
+};
+
+function max(a,b)
+{
+  if(a>b) return a;
+  else return b;
+};
+
+function isfull()
+{
+    for (var row = 0; row<3; row++)
+    {
+         for (var col = 0; col<3; col++) if(board[row][col]===0) return 0;
+    }
+return 1;
+};
+
+function evaluate(player)
+{
+    for (var row = 0; row<3; row++)
+    {
+        if (board[row][0]===board[row][1] && board[row][1]===board[row][2])
+        {
+            if (board[row][0]===player) return 10;
+            else if (board[row][0]===-player) return -10;
+        }
+    }
+    for (var col = 0; col<3; col++)
+    {
+        if (board[0][col]===board[1][col] && board[1][col]===board[2][col])
+        {
+            if (board[0][col]===player) return 10;
+            else if (board[0][col]===-player) return -10;
+        }
+    }
+  
+    if (board[0][0]===board[1][1] && board[1][1]===board[2][2])
+    {
+        if (board[0][0]===player) return 10;
+        else if (board[0][0]===-player) return -10;
+    }
+    if (board[0][2]===board[1][1] && board[1][1]===board[2][0])
+    {
+        if (board[0][2]===player) return 10;
+        else if (board[0][2]===-player) return -10;
+    }
+    return 0;
+};
+
+function minimax(isMax)
+{
+    var score = evaluate(favour);
+    if(score===10) return 10;
+    else if(score===-10) return -10;
+    else if(isfull()===1 && score===0) return 0; 
+    
+    var tmp=player; 
+    if(isMax)
+    {
+        var best = -1000;
+        for (var i = 0; i<3; i++)
+        {
+            for (var j = 0; j<3; j++)
+            {
+                if (board[i][j]===0)
+                {
+                    player=tmp;
+                    board[i][j] = player;
+                    player*=-1;
+                    best = max(best,minimax(!isMax));
+                    board[i][j] = 0;
+                }
+            }
+        }
+        return best;
+    }
+ 
+    else
+    {
+        var worst = 1000;
+        for (var i = 0; i<3; i++)
+        {
+            for (var j = 0; j<3; j++)
+            {
+
+                if (board[i][j]===0)
+                {
+                   player=tmp;
+                    board[i][j] = player;
+                     player*=-1;
+                    worst = min(worst,minimax(!isMax));
+                    board[i][j] = 0;
+                }
+            }
+        }
+        return worst;
+    }
+};
+
+function findBestMove()
+{
+    var bestVal = -1000,row=-1,col=-1;
+    var tmp=player;
+    for (var i = 0; i<3; i++)
+    {
+        for (var j = 0; j<3; j++)
+        {
+            player=tmp;
+            if (board[i][j]===0)
+            {
+                board[i][j]=player;
+                favour=player;
+                player*=-1;
+                var moveVal = minimax(0);
+                board[i][j] = 0;
+                if (moveVal >= bestVal) row=i,col = j,bestVal = moveVal;   
+            }
+        }
+    }
+    player=tmp;
+    board[row][col]=player;  
+    if(player===1) cells[3*row+col].classList.add('x');
+    else cells[3*row+col].classList.add('o');
+};
+
+
 
 const aifunc = () => {
   ai=0;
@@ -37,13 +170,17 @@ const aifunc2 = () => {
 
 const XX = () => {
   xchance=true;
+  ini=1;
   modal2.style.display="none";
   statusupdate();
+ 
 };
 
 const OO = () => {
-  xchance=false;
+  xchance=true;
   modal2.style.display="none";
+  ini=-1;
+  if(chance===1 && ai===1) cmove();
   statusupdate();
 };
 
@@ -60,8 +197,17 @@ const handleWin = (letter) => {
 };
 
 const statusupdate = () =>{
-	if(!xchance) status.innerHTML="Current Chance: O";
-	else status.innerHTML="Current Chance: X";
+  if(ai===0)
+  {
+	if(xchance && ini===1) status.innerHTML="Current Chance: Player 1";
+  else if(xchance && ini===-1) status.innerHTML="Current Chance: Player 2";
+  else if(!xchance && ini===1) status.innerHTML="Current Chance: Player 2";
+	else if(!xchance && ini===-1) status.innerHTML="Current Chance: Player 1";
+  }
+  else if(ai===1)
+  {
+  status.innerHTML="Current Chance: Player";
+  }
 };
 
 const checkGameStatus = () => {
@@ -133,28 +279,30 @@ const checkGameStatus = () => {
   } 
 };
 
-const change = (a) =>{
-	if(!(a.target.classList[2]==='x') && !(a.target.classList[2]=='o'))
-	{
-	if(xchance) a.target.classList.add('x');
-	else a.target.classList.add('o');
+function move(a){
+if(!(a.target.classList[2]==='x') && !(a.target.classList[2]=='o'))
+  {
+  if(xchance) a.target.classList.add('x');
+  else a.target.classList.add('o');
   }
   else return;
   if(xchance) board[Math.floor(a.target.classList[1]/3)][a.target.classList[1]%3]=1;
   else board[Math.floor(a.target.classList[1]/3)][a.target.classList[1]%3]=-1;
   xchance=!xchance;
-  
   checkGameStatus();
   statusupdate();
-  if(won===false && ai===1) 
-    {
-      if(xchance) player=1;
-      else player=-1;
-      findBestMove();
-      xchance=!xchance;
-      checkGameStatus();
-      statusupdate();
-    }
+}
+
+function cmove(){
+  if(xchance) player=1;
+  else player=-1;
+  findBestMove();
+  xchance=!xchance;
+  checkGameStatus();
+}
+const change = (a) =>{
+  if(ai===1) move(a),cmove();
+  if(ai===0) move(a);
 };
 
 opt1.addEventListener('click',aifunc)
@@ -164,5 +312,4 @@ O.addEventListener('click',OO)
 
 for(const cell of cells){
 	cell.addEventListener('click',change);
-
 };
